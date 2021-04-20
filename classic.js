c8c02653c8b59617f45e3791e27c0715564026da
ssivilotti <68237440+ssivilotti@users.noreply.github.com>
2021-04-20 19:06:24 -0400

    classic tic tac toe working

--- classic.js
+++ classic.js
@@ -1,88 +1,99 @@
 let playbutton = document.getElementById("play");
-//playbutton.addEventListener("click", createBoard);
+playbutton.addEventListener("click", resetBoard);
 let board = document.getElementById("board");
-
+let turn = 1;
+let boxArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+//add event listener to each button
 for (let i = 0; i < 9; i++) {
   let box = document.getElementById(i);
-  box.addEventListener("click", boxCheck(this.id));
+  box.addEventListener("click", (event) => boxCheck(event));
 }
 
-function boxCheck(id) {
-  console.log(id);
-  //   var boxClicked = "box" + boxNum;
-  //   if (turn === 0 || giantBoard[largeBoxID.substring(3, 4)] !== 0) {
-  //     hideElement(largeBoxID);
-  //     largeBoxID = boxClicked;
-  //   }
-  //   if (boxClicked == largeBoxID && board[boxNum * 9 + subBox] === 0 && on) {
-  //     showXO(subBox);
-  //     largeBoxID = "box" + subBox;
-  //     showElement(largeBoxID);
-  //     switchPlayers();
-  //   }
+//event listener for button
+function boxCheck(event) {
+  let box = event.srcElement;
+  let boxNum = box.id;
+  console.log(boxNum);
+  let image = document.createElement("img");
+  image.className = "square";
+  if (boxArray[boxNum] == 0) {
+    boxArray[boxNum] = turn;
+    if (turn === 1) {
+      image.src = "https://freetictactoe.com/images/mark-x.png";
+      //box.className = "square x";
+      turn = 2;
+    } else {
+      image.src =
+        "https://www.hospitalevangelico.com/images/diabetes_imagen.png";
+      turn = 1;
+    }
+    box.append(image);
+    console.log(box);
+    console.log(boxArray);
+  }
+  let win = checkWin();
+  console.log("win: " + win);
+  if (win != 0) {
+    announceWin(win);
+  }
 }
 
-//change small square to player's image
-function showXO(subBox) {
-  hideElement(largeBoxID);
-  var boxID;
-  if (subBox != 9) {
-    boxID = largeBoxID.substring(3, 4) + ":" + subBox;
+function checkWin() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      boxArray[i * 3] == boxArray[i * 3 + 1] &&
+      boxArray[i * 3 + 1] == boxArray[i * 3 + 2]
+    ) {
+      //check row win
+      if (boxArray[i * 3] != 0) {
+        return boxArray[i * 3];
+      }
+    } else if (
+      boxArray[i] == boxArray[i + 3] &&
+      boxArray[i + 3] == boxArray[i + 6]
+    ) {
+      //column win
+      if (boxArray[i] != 0) {
+        return boxArray[i];
+      }
+    }
+    //check diagonal wins
+    if (
+      (boxArray[0] == boxArray[4] && boxArray[4] == boxArray[8]) ||
+      (boxArray[2] == boxArray[4] && boxArray[4] == boxArray[6])
+    ) {
+      if (boxArray[4] != 0) {
+        return boxArray[4];
+      }
+    }
+  }
+  if (boxArray.includes(0)) {
+    return 0;
+  }
+  return -1;
+}
+
+function announceWin(win) {
+  //adds an announcement of who won below the board
+  let header = document.createElement("h1");
+  let winner = "";
+  if (win == -1) {
+    winner = "Tie";
   } else {
-    boxID = "box" + largeBoxID.substring(3, 4) + "XO";
+    winner = "Player " + win + " wins!";
   }
-  console.log(boxID);
-  if (playerNum == 1) {
-    setProperty(
-      boxID,
-      "image",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Not_allowed.svg/1107px-Not_allowed.svg.png"
-    );
-  } else {
-    setProperty(boxID, "image", "http://otranscribe.com/favicon.png");
-  }
-  showElement(boxID);
-  updateArray(subBox);
-  if (subBox != 9) {
-    checkWin();
-  }
+  header.innerHTML = winner;
+  document.getElementById("win").append(header);
 }
 
-//update the array of the specific box
-function updateArray(subBox) {
-  var boxNum = largeBoxID.substring(3, 4);
-  if (subBox != 9) {
-    board[9 * boxNum + subBox] = playerNum;
-    console.log(board);
-  } else {
-    giantBoard[boxNum] = playerNum;
-    console.log(giantBoard);
+function resetBoard() {
+  //clears images from buttons
+  for (let i = 0; i < 9; i++) {
+    let button = document.getElementById(i);
+    button.innerHTML = "";
   }
+  //resets play conditions
+  turn = 1;
+  boxArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  document.getElementById("win").innerHTML = "";
 }
-
-//switch who's turn it is
-function switchPlayers() {
-  if (playerNum == 1) {
-    hideElement("p1Box");
-    playerNum = 2;
-    showElement("p2Box");
-  } else {
-    hideElement("p2Box");
-    playerNum = 1;
-    showElement("p1Box");
-  }
-  turn++;
-  console.log(playerNum);
-}
-
-//function createBoard() {
-//for (i = 0; i < 9; i++) {
-//square = document.createElement("button");
-//square.id = i;
-//square.class = "square";
-//document.append(square);
-//if (i % 3 == 2) {
-//document.append(document.createElement("p"));
-//}
-//}
-//}
